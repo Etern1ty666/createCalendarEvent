c7f4f0c14faa7c4302904e6556a3d03437203083
etern1ty666 <novikov-reg@mail.ru>
2025-08-27 16:22:42 +0300

    update event if exists + new check

--- app.py
+++ app.py
@@ -29,27 +29,81 @@ TARGET_STAGES = [
 app = FastAPI()
 
 
-async def create_calendar_event_in_bitrix(start_date, end_date, title, deal_id, participants, object_passport_link):
+async def event_already_exists(event_deal, event_from, event_to):
+    # проверяем по ивентам в заданном промежутке и если в одном из ивентов есть совпадение на ID сделки, то считаем что ивент уже был создан на заданный промежуток
     async with httpx.AsyncClient() as client:
-        response = await client.post(
-            f"{BITRIX_WEBHOOK_URL}calendar.event.add",
+        resp = await client.post(
+            f"{BITRIX_WEBHOOK_URL}calendar.event.get",
             json={
+                "ownerId": TO_CALENDAR_OWNER_ID,
+                "type": "user",
+                "from": event_from,
+                "to": event_to
+            }
+        )
+        events = resp.json().get("result", [])
+        for event in events:
+            UF_CRM_CAL_EVENT = event.get("UF_CRM_CAL_EVENT") or []
+            result = [s.removeprefix("D_") for s in UF_CRM_CAL_EVENT if s.startswith("D_")]
+            if str(event_deal) in result:
+                return {'status': True, 'event_id': event.get("ID")}
+    return {'status': False, 'event_id': ''}
+
+
+async def update_calendar_event_in_bitrix(event_id, start_date, end_date, title, deal_id, participants, object_passport_link, should_skip_time='N'):
+    params = {
+                "id": int(event_id),
+                "from": start_date,
+                "to": end_date,
+                "name": f"{title}",
+                'crm_fields': [
+                    f'D_{deal_id}'
+                ],
                 "type": "user",
                 "ownerId": TO_CALENDAR_OWNER_ID,
                 "section": 44,
-                "name": f"Выезд на ТО, {title}",
-                "from": start_date,
-                "to": end_date,
                 "is_meeting": "Y",
                 "attendees": participants,
                 "host": TO_CALENDAR_OWNER_ID,
                 "remind": [{"type": "hour", "count": 1}, {"type": "day", "count": 1}],
                 "accessibility": "busy",
+                "description": f"Ссылка на паспорт объекта: {object_passport_link}\n\n *Создано автоматически*",
+                "skip_time": should_skip_time
+            }
+    print('calendar.event.update with params:', params)
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{BITRIX_WEBHOOK_URL}calendar.event.update",
+            json=params
+        )
+    return response
+
+
+
+async def create_calendar_event_in_bitrix(start_date, end_date, title, deal_id, participants, object_passport_link, should_skip_time='N'):
+    params = {
+                "from": start_date,
+                "to": end_date,
+                "name": f"{title}",
                 'crm_fields': [
                     f'D_{deal_id}'
                 ],
-                "description": f"Ссылка на паспорт объекта: {object_passport_link}"
+                "type": "user",
+                "ownerId": TO_CALENDAR_OWNER_ID,
+                "section": 44,
+                "is_meeting": "Y",
+                "attendees": participants,
+                "host": TO_CALENDAR_OWNER_ID,
+                "remind": [{"type": "hour", "count": 1}, {"type": "day", "count": 1}],
+                "accessibility": "busy",
+                "description": f"Ссылка на паспорт объекта: {object_passport_link}\n\n *Создано автоматически*",
+                "skip_time": should_skip_time
             }
+    print('calendar.event.add with params:', params)
+    async with httpx.AsyncClient() as client:
+        response = await client.post(
+            f"{BITRIX_WEBHOOK_URL}calendar.event.add",
+            json=params
         )
     return response
 
@@ -58,7 +112,8 @@ async def create_calendar_event_in_bitrix(start_date, end_date, title, deal_id, 
 async def create_calendar_event(request: Request):
     body = (await request.body()).decode("utf-8")
     data = urllib.parse.parse_qs(body)
-    print('data:', data)
+    print('Recieved data:', data)
+
     received_auth_token = data.get("auth[application_token]", [""])[0]
     if received_auth_token != BITRIX_APP_AUTH_TOKEN:
         print('Access denied')
@@ -80,9 +135,7 @@ async def create_calendar_event(request: Request):
             json={"entityTypeId": entity_type_id, "id": item_id}
         )
         item = resp.json().get("result", {}).get("item", {})
-        print(resp.json())
-        for i in item:
-            print(i, item[i])
+        print('CRM item:', resp.json())
 
     current_stage = item.get("stageId") # jan feb mar etc...
     if current_stage not in TARGET_STAGES:
@@ -102,7 +155,7 @@ async def create_calendar_event(request: Request):
             json={"id": deal_id}
         )
         item = response.json().get("result", {})
-        print(item)
+        print('Привязанная сделка:', item)
     responsible_user_id = item.get('ASSIGNED_BY_ID')
     title = item.get("TITLE")
 
@@ -112,8 +165,6 @@ async def create_calendar_event(request: Request):
     participants.append(second_engineer_id)
     participants.append(responsible_user_id)
 
-    print(f'Event: Выезд на ТО, {title} from {start_date} to {end_date} with: [{responsible_user_id, engineer_users_id, second_engineer_id}]')
-
     fmt = "%Y-%m-%dT%H:%M:%S%z"
 
     start_dt = datetime.strptime(start_date, fmt)
@@ -121,52 +172,31 @@ async def create_calendar_event(request: Request):
 
     if start_dt.hour == 0 and start_dt.minute == 0 and start_dt.second == 0:
         start_dt = start_dt.replace(hour=8, minute=0, second=0)
+        print('new start date:', start_dt)
 
-    if (end_dt.date() == start_dt.date() and
-            end_dt.hour == 0 and end_dt.minute == 0 and end_dt.second == 0):
+    if end_dt.date() == start_dt.date() and end_dt.hour == 0 and end_dt.minute == 0 and end_dt.second == 0:
         end_dt = end_dt.replace(hour=21, minute=0, second=0)
+        print('new end date:', end_dt)
 
     start_date = start_dt.isoformat()
     end_date = end_dt.isoformat()
-    print('new dates:', start_date, end_date, type(start_date), type(end_date))
 
-    '''
-    def parse_date(date_str):
-    try:
-        return datetime.strptime(date_str, "%d.%m.%Y %H:%M:%S")
-    except ValueError:
-        return datetime.fromisoformat(date_str).replace(tzinfo=None)
-    #проверка на дубликат такого же ивента, но если раскоментировать то надо еще проверять по названию
-    async with httpx.AsyncClient() as client:
-        resp = await client.post(
-            f"{BITRIX_WEBHOOK_URL}calendar.event.get",
-            json={
-                "ownerId": responsible_user_id,
-                "type": "user",
-                "from": start_date,
-                "to": end_date
-            }
-        )
-        events = resp.json().get("result", [])
-        expected_start = parse_date(start_date)
-        expected_end = parse_date(end_date)
+    result = await event_already_exists(event_deal=item.get('ID'), event_from=start_date ,event_to=end_date)
+    if result['status'] == True:
+        print('Edit event')
+        response = await update_calendar_event_in_bitrix(event_id=result['event_id'], start_date=start_date, end_date=end_date, title=title,
+                                                         deal_id=deal_id, participants=participants,
+                                                         object_passport_link=object_passport_link)
+        print('calendar.event.update response:', response.text)
 
-        for event in events:
-            event_start = parse_date(event["DATE_FROM"])
-            event_end = parse_date(event["DATE_TO"])
-
-            print(
-                f"{event['DATE_FROM']} == {start_date} → {event_start == expected_start}; "
-                f"{event['DATE_TO']} == {end_date} → {event_end == expected_end}"
-            )
-
-            if event_start == expected_start and event_end == expected_end:
-                print('Event already exists')
-                return {"status": "ok", "message": "Event already exists"}'''
+        if response.status_code == 200:
+            return {"status": "ok", "message": "Event updated"}
+        else:
+            return {"status": "error", "message": f'update error {response.text}'}
 
 
-    print(f'Create with data: start_date={start_date} | end_date={end_date} | title={title} | deal_id={deal_id} | participants={participants} | object_passport_link={object_passport_link}')
     response = await create_calendar_event_in_bitrix(start_date=start_date, end_date=end_date, title=title, deal_id=deal_id, participants=participants, object_passport_link=object_passport_link)
+    print('calendar.event.add response:', response.text)
     if response.status_code == 200:
         return {"status": "ok", "message": "Event created"}
     else:
